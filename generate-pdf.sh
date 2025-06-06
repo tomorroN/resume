4d29cadb64d1f654cccaf1c070c1a044eccf85db
dimanikitiuk <dima@ownplay.co>
2025-06-07 00:46:20 +0300

    Enhance PDF generation script with additional formatting options

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,1 +1,11 @@
-pandoc README.md -o resume.pdf
+#!/bin/bash
+pandoc README.md -o resume.pdf \
+  --pdf-engine=xelatex \
+  -V mainfont="Fira Code" \
+  -V fontsize=11pt \
+  -V geometry=margin=1in \
+  -V colorlinks=true \
+  -V linkcolor=blue \
+  -V pagestyle=empty \
+  -V linespread=1.15 \
+  -V spacing=half
